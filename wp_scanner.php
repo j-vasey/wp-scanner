823cf33aec5a03bcbda536cfe390b29b5603bb6b
Freshsploit <info@joshvasey.co.uk>
2023-07-18 23:39:31 +0100

    Git created

---
+++ wp_scanner.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WordPress Cloud AV Scanner
+ * Plugin URI: https://joshvasey.co.uk
+ * Description: Josh's WordPress Cloud Antivirus Scanner
+ * Version: 0.1
+ * Author: Joshua Vasey
+ * Author URI: https://joshvasey.co.uk
+ **/
